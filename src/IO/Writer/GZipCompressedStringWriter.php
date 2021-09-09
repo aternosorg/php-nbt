60b5bc65dbfa99c8afef2fc5df3b23cc909056b1
Kurt Thiemann <kurt@aternos.org>
2021-09-09 16:21:17 +0200

    fix GZipCompressedStringWriter

--- src/IO/Writer/GZipCompressedStringWriter.php
+++ src/IO/Writer/GZipCompressedStringWriter.php
@@ -6,6 +6,6 @@ class GZipCompressedStringWriter extends StringWriter
 {
     public function getStringData(): string
     {
-        return gzencode(parent::getStringData(), ZLIB_ENCODING_DEFLATE);
+        return gzencode(parent::getStringData());
     }
 }
