862406eb4f137cca849fbf50feb20b1ad0b07a4c
Kurt Thiemann <kurt@aternos.org>
2024-08-13 17:32:37 +0200

    allow setting maximum decompressed size for ZLibCompressedStringReader and GZipCompressedStringReader

--- src/IO/Reader/GZipCompressedStringReader.php
+++ src/IO/Reader/GZipCompressedStringReader.php
@@ -9,9 +9,9 @@ class GZipCompressedStringReader extends StringReader
     /**
      * @throws Exception
      */
-    public function __construct(string $data, int $format)
+    public function __construct(string $data, int $format, int $maxDecompressedSize = 0)
     {
-        if (($uncompressed = @gzdecode($data)) === false) {
+        if (($uncompressed = @gzdecode($data, $maxDecompressedSize)) === false) {
             throw new Exception("Invalid GZip data");
         }
         parent::__construct($uncompressed, $format);
--- src/IO/Reader/ZLibCompressedStringReader.php
+++ src/IO/Reader/ZLibCompressedStringReader.php
@@ -9,9 +9,9 @@ class ZLibCompressedStringReader extends StringReader
     /**
      * @throws Exception
      */
-    public function __construct(string $data, int $format)
+    public function __construct(string $data, int $format, int $maxDecompressedSize = 0)
     {
-        if (($uncompressed = @zlib_decode($data)) === false) {
+        if (($uncompressed = @zlib_decode($data, $maxDecompressedSize)) === false) {
             throw new Exception("Invalid ZLib data");
         }
         parent::__construct($uncompressed, $format);
